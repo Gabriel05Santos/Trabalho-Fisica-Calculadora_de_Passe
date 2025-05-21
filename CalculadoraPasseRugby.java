2474b85bc2753dbc0be125d6145f94bf6210d597
GABRIEL HENRIQUE SANTOS <gabrielrumoatitacvfullzaco@gmail.com>
2025-05-21 18:29:43 -0300

    Add files via upload

---
+++ CalculadoraPasseRugby.java
@@ -0,0 +1,12 @@
+package com.mycompany.calculadorapasserugby;
+
+/**
+ *
+ * @author gab-linux
+ */
+public class CalculadoraPasseRugby {
+
+     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new CalculadoraPasseRugby().setVisible(true));
+    }
+}
